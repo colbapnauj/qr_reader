0f2f5174b543aa14940ee69eb1c4f74890502e1f
Jean Chiclla <j-c_f@outlook.com>
2021-07-15 09:08:13 -0500

    Día 3

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -87,4 +87,25 @@ class DBProvider {
 
     return res.isNotEmpty ? res.map((s) => ScanModel.fromJson(s)).toList() : [];
   }
+
+  Future<int> updateScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db.update('Scans', nuevoScan.toJson(),
+        where: 'id = ?', whereArgs: [nuevoScan.id]);
+    return res;
+  }
+
+  Future<int> deleteScan(int id) async {
+    final db = await database;
+    final res = await db.delete('Scans', where: 'id = ?', whereArgs: [id]);
+    return res;
+  }
+
+  Future<int> deleteAllScans() async {
+    final db = await database;
+    final res = await db.rawDelete('''
+      DELETE FROM Scans
+    ''');
+    return res;
+  }
 }
--- lib/providers/scan_list_provider.dart
+++ lib/providers/scan_list_provider.dart
@@ -6,7 +6,32 @@ class ScanListProvider extends ChangeNotifier {
 
   String tipoSeleccionado = 'http';
 
-  nuevoScan(String valor) {
+  nuevoScan(String valor) async {
     final nuevoScan = new ScanModel(valor: valor);
+    final id = await DBProvider.db.nuevoScan(nuevoScan);
+    // Asignar el ID de la base de datos al modelo
+    nuevoScan.id = id;
+
+    if (this.tipoSeleccionado == nuevoScan.tipo) {
+      this.scans.add(nuevoScan);
+      notifyListeners();
+    }
+  }
+
+  cargarScans() async {
+    final scans = await DBProvider.db.getTodosLosScans();
+    this.scans = [...scans];
+    notifyListeners();
+  }
+
+  cargarScansPorTipo(String tipo) async {
+    final scans = await DBProvider.db.getTodosPorTipo(tipo);
+    this.scans = [...scans];
+    this.tipoSeleccionado = tipo;
+    notifyListeners();
+  }
+
+  borrarTodos() async {
+    // await DBProvider.db.
   }
 }
